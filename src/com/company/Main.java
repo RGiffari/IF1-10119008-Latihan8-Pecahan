899e2df42946b2dcd31e0bfab52efd799a29d789
RGiffari <11R02Giffari@users.noreply.github.com>
2020-10-20 23:58:39 +0700

    initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+/**
+ *
+ * @author
+ * NAMA         : Raihan Giffari
+ * KELAS        : PBO12
+ * NIM          : 10119008
+ * Deskripsi Program    : Program ini berisi tentang pecahan
+ *
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+	    float f;
+	    double d;
+	    f = 1234567890.123456789F;
+	    d = 1_234_567_890.123456789;
+	    System.out.println("Float : " + f);
+	    System.out.println("Double : " + d);
+    }
+}
